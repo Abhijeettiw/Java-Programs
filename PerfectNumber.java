e626e34cc3551daae6bd24df3bfbae22e32b54c3
Abhijeet Tiwari <122159935+Abhijeettiw@users.noreply.github.com>
2023-02-08 18:21:46 +0530

    Update PerfectNumber.java

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,37 +1,33 @@
 // Perfect Number
-
 import java.util.Scanner;
+public class main {
 
-public class Perfectnumber
-{
-	public Perfectnumber() {
-	}
+        public static void main(String[] args)
+        {
+            int a=0;
+            Scanner s = new Scanner(System.in);15
+            System.out.println("Enter the number :");
+            a=s.nextInt();
+            int b=a-1;
+            int sum=0;
 
-	public static void main(String[] args)
-	{
-		int a=0;
-		try (Scanner s = new Scanner(System.in)) {
-			System.out.println("Enter the number :");
-			a=s.nextInt();
-		}
-		int b=a-1;
-		int sum=0;
-		
-		for(int i =1;i<=b;i++)
-		{
-			if(a%i==0)
-			{
-				sum += i;
-			}
-		}
-		
-		if(sum==a)
-		{
-			System.out.println("True");
-		}
-		else
-		{	
-			System.out.println("False");
-		}
-	}
+            for(int i =1;i<=b;i++)
+            {
+                if(a%i==0)
+                {
+                    sum += i;
+                }
+            }
+
+            if(sum==a)
+            {
+                System.out.println("True");
+            }
+            else
+            {
+                System.out.println("False");
+            }
+        }
+
 }
+
